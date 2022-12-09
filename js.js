2bada4de2dcf74f255f0082f3482f0014c9fda4f
Małgorzata Mikła <106150150+goszzi@users.noreply.github.com>
2022-12-09 23:58:59 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,63 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const cityName = document.querySelector('.city-name')
+const warning = document.querySelector('.warning')
+const photo = document.querySelector('.photo')
+const weather = document.querySelector('.weather')
+const temperature = document.querySelector('.temperature')
+const humidity = document.querySelector('.humidity')
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
+const API_KEY = '&appid=9517604b7ce83aae2a0c90867b92d9d9'
+const API_UNITS = '&units=metric'
+
+const getWeather = () => {
+	const city = input.value || 'London'
+	const URL = API_LINK + city + API_KEY + API_UNITS
+
+	axios
+		.get(URL)
+		.then(res => {
+			const temp = res.data.main.temp
+			const hum = res.data.main.humidity
+			const status = Object.assign({}, ...res.data.weather)
+
+			cityName.textContent = res.data.name
+			temperature.textContent = Math.floor(temp) + '℃'
+			humidity.textContent = hum + '%'
+			weather.textContent = status.main
+
+			warning.textContent = ''
+			input.value = ''
+
+			// temperature.textContent = Math.floor(res.data.main.temp) + '℃'
+			// humidity.textContent = res.data.main.humidity + '%'
+
+			if (status.id >= 200 && status.id < 300) {
+				photo.setAttribute('src', './img/thunderstorm.png')
+			} else if (status.id >= 300 && status.id < 400) {
+				photo.setAttribute('src', './img/drizzle.png')
+			} else if (status.id >= 500 && status.id < 600) {
+				photo.setAttribute('src', './img/rain.png')
+			} else if (status.id >= 600 && status.id < 700) {
+				photo.setAttribute('src', './img/ice.png')
+			} else if (status.id >= 700 && status.id < 800) {
+				photo.setAttribute('src', './img/fog.png')
+			} else if (status.id == 800) {
+				photo.setAttribute('src', './img/sun.png')
+			} else if (status.id > 800 && status.id < 900) {
+				photo.setAttribute('src', './img/cloud.png')
+			} else {
+				photo.setAttribute('src', './img/unknown.png')
+			}
+		})
+		.catch(() => (warning.textContent = 'Enter correct city name'))
+}
+
+const enterCheck = e => {
+	if (e.key === 'Enter') getWeather()
+}
+
+input.addEventListener('keyup', enterCheck)
+getWeather()
+button.addEventListener('click', getWeather)
